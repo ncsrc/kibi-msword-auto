082f4fafa3001d37dfa38830e541048ae2134ce2
Ncat <ncat@local.com>
2016-11-12 19:40:16 +0300

    adds test to Student DAO

---
+++ src/test/java/ru/tstu/msword_automation/database/StudentDaoTest.java
@@ -0,0 +1,53 @@
+package ru.tstu.msword_automation.database;
+
+
+import ru.tstu.msword_automation.database.datasets.Student;
+
+import java.sql.SQLException;
+
+public class StudentDaoTest extends AbstractDaoTest<StudentDao, Student, Integer>
+{
+
+	// TODO implenet class
+
+	@Override
+	protected Student getDataset()
+	{
+		return new Student(1, "Иван", "Иванов", "Иванович",
+				"бакалавр", "11.11.11.11");
+	}
+
+	@Override
+	protected Student getSecondDataset()
+	{
+		return new Student(2, "qwe", "test", "test",
+				"sdtest", "11.11.11.11");
+	}
+
+	@Override
+	protected StudentDao getDao() throws SQLException
+	{
+		return DatabaseService.getInstance().getStudentDao();
+	}
+
+	@Override
+	protected Integer getPkOfFirst()
+	{
+		return 1;
+	}
+
+	@Override
+	protected Integer getPkOfSecond()
+	{
+		return 2;
+	}
+
+	@Override
+	protected Integer getNonExistentPk()
+	{
+		return -1;
+	}
+
+
+
+}
