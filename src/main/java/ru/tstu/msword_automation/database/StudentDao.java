70ab1652500b4fa9c9c6d76b6a091780ae37fda2
Ncat <ncat@local.com>
2016-11-12 19:29:25 +0300

    implements StudentDao

--- src/main/java/ru/tstu/msword_automation/database/StudentDao.java
+++ src/main/java/ru/tstu/msword_automation/database/StudentDao.java
@@ -3,53 +3,113 @@ package ru.tstu.msword_automation.database;
 
 import ru.tstu.msword_automation.database.datasets.Student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-class StudentDao implements Dao<Student, Integer>
+class StudentDao extends AbstractDao<Student, Integer>
 {
-	private final ConnectionPool connectionPool;
 
 
 	StudentDao(ConnectionPool pool)
 	{
-		this.connectionPool = pool;
+		super(pool);
+	}
+
+
+	@Override
+	protected PreparedStatement getCreationStatement(Connection connection, Student dataset) throws SQLException
+	{
+		PreparedStatement statement = connection.prepareStatement("INSERT INTO Students VALUES(?, ?, ?, ?, ?, ?)");
+		statement.setInt(1, dataset.getId());
+		statement.setString(2, dataset.getFirstName());
+		statement.setString(3, dataset.getLastName());
+		statement.setString(4, dataset.getMiddleName());
+		statement.setString(5, dataset.getQualification());
+		statement.setString(6, dataset.getCourseCode());
+
+		return statement;
 	}
 
 	@Override
-	public void create(Student dataset) throws SQLException
+	protected PreparedStatement getReadingByPkStatement(Connection connection, Integer pk) throws SQLException
 	{
+		PreparedStatement statement = connection.prepareStatement("SELECT student_id, first_name, last_name, middle_name, qualification, course_code " +
+				"FROM Students WHERE student_id = ?");
+		statement.setInt(1, pk);
 
+		return statement;
 	}
 
 	@Override
-	public Student read(Integer pk) throws SQLException
+	protected PreparedStatement getReadingAllStatement(Connection connection) throws SQLException
 	{
+		return connection.prepareStatement("SELECT student_id, first_name, last_name, middle_name, qualification, course_code FROM Students");
+	}
+
+	@Override
+	protected PreparedStatement getUpdateStatement(Connection connection, Student dataset, Integer pk) throws SQLException
+	{
+		PreparedStatement statement = connection.prepareStatement("UPDATE Students SET student_id = ?, first_name = ?, last_name = ?, middle_name = ?, qualification = ?," +
+				"  course_code = ? WHERE student_id = ?");
+		statement.setInt(1, dataset.getId());
+		statement.setString(2, dataset.getFirstName());
+		statement.setString(3, dataset.getLastName());
+		statement.setString(4, dataset.getMiddleName());
+		statement.setString(5, dataset.getQualification());
+		statement.setString(6, dataset.getCourseCode());
+		statement.setInt(7, pk);
+
 		return null;
 	}
 
 	@Override
-	public List<Student> readAll() throws SQLException
+	protected PreparedStatement getDeleteByPkStatement(Connection connection, Integer pk) throws SQLException
 	{
-		return null;
+		PreparedStatement statement = connection.prepareStatement("DELETE FROM Students WHERE student_id = ?");
+		statement.setInt(1, pk);
+
+		return statement;
 	}
 
 	@Override
-	public void update(Integer pk, Student dataset) throws SQLException
+	protected PreparedStatement getDeleteAllStatement(Connection connection) throws SQLException
 	{
-
+		return connection.prepareStatement("DELETE FROM Students");
 	}
 
 	@Override
-	public void delete(Integer pk) throws SQLException
+	protected List<Student> parseResultSet(ResultSet resultSet) throws SQLException
 	{
+		List<Student> list = new ArrayList<>();
+		while(resultSet.next()){
+			list.add(new Student(resultSet.getInt("student_id"), resultSet.getString("first_name"), resultSet.getString("last_name"),
+					resultSet.getString("middle_name"), resultSet.getString("qualification"), resultSet.getString("course_code")));
+		}
 
+		return list;
 	}
-
-	@Override
-	public void deleteAll() throws SQLException
-	{
-
-	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
