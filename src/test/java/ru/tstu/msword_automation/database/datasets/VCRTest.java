eb7202ac9fc2e6e7346640fb545ab12899fdda60
Ncat <ncat@local.com>
2016-11-14 19:00:12 +0300

    fixes error

--- src/test/java/ru/tstu/msword_automation/database/datasets/VCRTest.java
+++ src/test/java/ru/tstu/msword_automation/database/datasets/VCRTest.java
@@ -10,9 +10,9 @@ public class VCRTest
 	@Test
 	public void equalsCorrectness() throws Exception
 	{
-		VCR vcr0 = new VCR("test", "test1", "test2");
-		VCR vcr1 = new VCR("test", "test1", "test2");
-		VCR vcr2 = new VCR("test", "11test1", "test2asd");
+		VCR vcr0 = new VCR(1, "test", "test1", "test2");
+		VCR vcr1 = new VCR(1, "test", "test1", "test2");
+		VCR vcr2 = new VCR(2, "test", "11test1", "test2asd");
 
 		assertEquals(vcr0, vcr1);
 		assertNotEquals(vcr1, vcr2);
