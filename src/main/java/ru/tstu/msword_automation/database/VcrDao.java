17b1a22eb9b5c5ebb38cd628366facb7b78a2bd7
Ncat <ncat@local.com>
2016-11-14 18:51:57 +0300

    implements VcrDao

--- src/main/java/ru/tstu/msword_automation/database/VcrDao.java
+++ src/main/java/ru/tstu/msword_automation/database/VcrDao.java
@@ -3,52 +3,89 @@ package ru.tstu.msword_automation.database;
 
 import ru.tstu.msword_automation.database.datasets.VCR;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-class VcrDao implements Dao<VCR, String>
+class VcrDao extends AbstractDao<VCR, String>
 {
-	private final ConnectionPool connectionPool;
+
 
 	VcrDao(ConnectionPool connectionPool)
 	{
-		this.connectionPool = connectionPool;
+		super(connectionPool);
+	}
+
+
+	@Override
+	protected PreparedStatement getCreationStatement(Connection connection, VCR dataset) throws SQLException
+	{
+		PreparedStatement statement = connection.prepareStatement("INSERT INTO VCRs VALUES(?, ?, ?, ?)");
+		statement.setInt(1, dataset.getStudentId());
+		statement.setString(2, dataset.getName());
+		statement.setString(3, dataset.getHeadName());
+		statement.setString(4, dataset.getReviewerName());
+		return statement;
 	}
 
 	@Override
-	public void create(VCR dataset) throws SQLException
+	protected PreparedStatement getReadingByPkStatement(Connection connection, String pk) throws SQLException
 	{
-
+		PreparedStatement statement = connection.prepareStatement("SELECT student_id, vcr_name, vcr_head, vcr_reviewer FROM VCRs WHERE vcr_name = ?");
+		statement.setString(1, pk);
+		return statement;
 	}
 
 	@Override
-	public VCR read(String pk) throws SQLException
+	protected PreparedStatement getReadingAllStatement(Connection connection) throws SQLException
 	{
-		return null;
+		return connection.prepareStatement("SELECT student_id, vcr_name, vcr_head, vcr_reviewer FROM VCRs");
 	}
 
 	@Override
-	public List<VCR> readAll() throws SQLException
+	protected PreparedStatement getUpdateStatement(Connection connection, VCR dataset, String pk) throws SQLException
 	{
-		return null;
+		PreparedStatement statement = connection.prepareStatement("UPDATE VCRs SET student_id = ?, vcr_name = ?, vcr_head = ?, vcr_reviewer = ? WHERE vcr_name = ?");
+		statement.setInt(1, dataset.getStudentId());
+		statement.setString(2, dataset.getName());
+		statement.setString(3, dataset.getHeadName());
+		statement.setString(4, dataset.getReviewerName());
+		statement.setString(5, pk);
+		return statement;
 	}
 
 	@Override
-	public void update(String pk, VCR dataset) throws SQLException
+	protected PreparedStatement getDeleteByPkStatement(Connection connection, String pk) throws SQLException
 	{
-
+		PreparedStatement statement = connection.prepareStatement("DELETE FROM VCRs WHERE vcr_name = ?");
+		statement.setString(1, pk);
+		return statement;
 	}
 
 	@Override
-	public void delete(String pk) throws SQLException
+	protected PreparedStatement getDeleteAllStatement(Connection connection) throws SQLException
 	{
-
+		return connection.prepareStatement("DELETE FROM VCRs");
 	}
 
 	@Override
-	public void deleteAll() throws SQLException
+	protected List<VCR> parseResultSet(ResultSet resultSet) throws SQLException
 	{
+		List<VCR> list = new ArrayList<>();
+		while(resultSet.next()){
+			list.add(new VCR(resultSet.getInt("student_id"), resultSet.getString("vcr_name"),
+					resultSet.getString("vcr_head"), resultSet.getString("vcr_reviewer")));
+		}
 
+		return list;
 	}
+
 
 }
+
+
+
+// TODO add interface ForeignKeyReadable - some tables can be read only by fk
