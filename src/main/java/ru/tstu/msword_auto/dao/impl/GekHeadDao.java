6b03c5185914c30e666c15fd8b44aa24c0efc43e
ncat <ncat@fakemail.com>
2017-03-25 18:11:01 +0300

    Add new working dao

--- src/main/java/ru/tstu/msword_auto/dao/impl/GekHeadDao.java
+++ src/main/java/ru/tstu/msword_auto/dao/impl/GekHeadDao.java
@@ -1,104 +1,159 @@
-//package ru.tstu.msword_auto.dao;
-//
-//
-//import ru.tstu.msword_auto.entity.GekHead;
-//
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//// TODO remove Connection from arguments, since protected
-//// TODO add getByCourseName() to dao
-//
-//public class GekHeadDao extends AbstractDao<GekHead, String> {
-//
-//	public GekHeadDao() {
-//		super();
-//	}
-//
-//
-//	@Override
-//	protected PreparedStatement getCreationStatement(Connection connection, GekHead dataset) throws SQLException {
-//		PreparedStatement statement = connection.prepareStatement("INSERT INTO Gek_Head VALUES(?, ?, ?)");
-//		statement.setString(1, dataset.getHead());
-//		statement.setString(2, dataset.getSubhead());
-//		statement.setString(3, dataset.getSecretary());
-//		return statement;
-//	}
-//
-//	@Override
-//	protected PreparedStatement getReadingByPkStatement(Connection connection, String pk) throws SQLException {
-//		PreparedStatement statement = connection.prepareStatement("SELECT gek_head, gek_subhead, gek_secretary FROM Gek_Head WHERE gek_head = ?");
-//		statement.setString(1, pk);
-//
-//		return statement;
-//	}
-//
-//	@Override
-//	protected PreparedStatement getReadingAllStatement(Connection connection) throws SQLException {
-//		return connection.prepareStatement("SELECT gek_head, gek_subhead, gek_secretary FROM Gek_Head");
-//	}
-//
-//	@Override
-//	protected PreparedStatement getUpdateStatement(Connection connection, GekHead dataset, String pk) throws SQLException {
-//		PreparedStatement statement = connection.prepareStatement("UPDATE Gek_Head SET gek_head = ?, gek_subhead = ?, gek_secretary = ? WHERE gek_head = ?");
-//		statement.setString(1, dataset.getHead());
-//		statement.setString(2, dataset.getSubhead());
-//		statement.setString(3, dataset.getSecretary());
-//		statement.setString(4, pk);
-//
-//		return statement;
-//	}
-//
-//	@Override
-//	protected PreparedStatement getDeleteByPkStatement(Connection connection, String pk) throws SQLException {
-//		PreparedStatement statement = connection.prepareStatement("DELETE FROM Gek_Head WHERE gek_head = ?");
-//		statement.setString(1, pk);
-//
-//		return statement;
-//	}
-//
-//	@Override
-//	protected PreparedStatement getDeleteAllStatement(Connection connection) throws SQLException {
-//		return connection.prepareStatement("DELETE FROM Gek_Head");
-//	}
-//
-//	@Override
-//	protected List<GekHead> parseResultSet(ResultSet resultSet) throws SQLException {
-//		List<GekHead> list = new ArrayList<>();
-//		while(resultSet.next()) {
-//			GekHead gek = new GekHead(resultSet.getString("gek_head"), resultSet.getString("gek_subhead"), resultSet.getString("gek_secretary"));
-//			list.add(gek);
-//		}
-//
-//		return list;
-//	}
-//
-//
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+package ru.tstu.msword_auto.dao.impl;
+
+
+import ru.tstu.msword_auto.dao.exceptions.DaoSystemException;
+import ru.tstu.msword_auto.dao.exceptions.NoSuchEntityException;
+import ru.tstu.msword_auto.entity.GekHead;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GekHeadDao extends AbstractDao<GekHead, Integer> {
+
+    // sql queries
+    static final String SQL_CREATE = "INSERT INTO GEK_HEAD(COURSE_NAME, GEK_HEAD, GEK_SUBHEAD, GEK_SECRETARY) VALUES(?, ?, ?, ?)";
+
+    static final String SQL_READ_BY_PK = "SELECT GEK_ID, COURSE_NAME, GEK_HEAD, GEK_SUBHEAD, GEK_SECRETARY FROM GEK_HEAD WHERE GEK_ID=?";
+
+    static final String SQL_READ_BY_COURSENAME = "SELECT GEK_ID, COURSE_NAME, GEK_HEAD, GEK_SUBHEAD, GEK_SECRETARY FROM GEK_HEAD WHERE COURSE_NAME=?";
+
+    static final String SQL_READ_ALL = "SELECT GEK_ID, COURSE_NAME, GEK_HEAD, GEK_SUBHEAD, GEK_SECRETARY FROM GEK_HEAD";
+
+    static final String SQL_UPDATE = "UPDATE GEK_HEAD SET COURSE_NAME=?, GEK_HEAD=?, GEK_SUBHEAD=?, GEK_SECRETARY=? WHERE GEK_ID=?";
+
+    static final String SQL_DELETE_BY_PK = "DELETE FROM GEK_HEAD WHERE GEK_ID=?";
+
+    static final String SQL_DELETE_ALL = "DELETE FROM GEK_HEAD";
+
+    // table column names
+    static final String TABLE_GEK_ID = "GEK_ID";
+    static final String TABLE_COURSE_NAME = "COURSE_NAME";
+    static final String TABLE_GEK_HEAD = "GEK_HEAD";
+    static final String TABLE_GEK_SUBHEAD = "GEK_SUBHEAD";
+    static final String TABLE_GEK_SECRETARY = "GEK_SECRETARY";
+
+
+	public GekHeadDao() {
+		super();
+	}
+
+
+
+	public GekHead readByCourseName(String courseName) throws DaoSystemException, NoSuchEntityException {
+		try {
+			PreparedStatement statement = connection.prepareStatement(SQL_READ_BY_COURSENAME);
+			statement.setString(1, courseName);
+			ResultSet resultSet = statement.executeQuery();
+			List<GekHead> entities = parseResultSet(resultSet);
+
+			if(entities.isEmpty()) {
+				throw new NoSuchEntityException();
+			}
+
+			resultSet.close();
+			statement.close();
+
+			return entities.get(0);
+		} catch (SQLException e) {
+			throw new DaoSystemException(e);
+		}
+
+	}
+
+
+	@Override
+	protected PreparedStatement getCreationStatement(GekHead dataset) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement(SQL_CREATE);
+		statement.setString(1, dataset.getCourseName());
+        statement.setString(2, dataset.getHead());
+		statement.setString(3, dataset.getSubhead());
+		statement.setString(4, dataset.getSecretary());
+		return statement;
+	}
+
+	@Override
+	protected PreparedStatement getReadingByPkStatement(Integer pk) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement(SQL_READ_BY_PK);
+		statement.setInt(1, pk);
+
+		return statement;
+	}
+
+	@Override
+	protected PreparedStatement getReadingAllStatement() throws SQLException {
+		return connection.prepareStatement(SQL_READ_ALL);
+	}
+
+	@Override
+	protected PreparedStatement getUpdateStatement(GekHead dataset, Integer pk) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement(SQL_UPDATE);
+
+		statement.setString(1, dataset.getCourseName());
+        statement.setString(2, dataset.getHead());
+		statement.setString(3, dataset.getSubhead());
+		statement.setString(4, dataset.getSecretary());
+		statement.setInt(5, pk);
+
+		return statement;
+	}
+
+	@Override
+	protected PreparedStatement getDeleteByPkStatement(Integer pk) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement(SQL_DELETE_BY_PK);
+		statement.setInt(1, pk);
+
+		return statement;
+	}
+
+	@Override
+	protected PreparedStatement getDeleteAllStatement() throws SQLException {
+		return connection.prepareStatement(SQL_DELETE_ALL);
+	}
+
+	@Override
+	protected List<GekHead> parseResultSet(ResultSet resultSet) throws SQLException {
+		List<GekHead> list = new ArrayList<>();
+		while(resultSet.next()) {
+			GekHead gek = new GekHead(
+                    resultSet.getString(TABLE_COURSE_NAME),
+			        resultSet.getString(TABLE_GEK_HEAD),
+                    resultSet.getString(TABLE_GEK_SUBHEAD),
+                    resultSet.getString(TABLE_GEK_SECRETARY)
+            );
+			gek.setGekId(resultSet.getInt(TABLE_GEK_ID));
+
+			list.add(gek);
+		}
+
+		return list;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/ru/tstu/msword_auto/dao/impl/GekHeadDaoTest.java
+++ src/test/java/ru/tstu/msword_auto/dao/impl/GekHeadDaoTest.java
@@ -1,55 +1,326 @@
-//package ru.tstu.msword_auto.dao;
-//
-//
-//import org.junit.Ignore;
-//import ru.tstu.msword_auto.entity.GekHead;
-//
-//
-//import java.sql.SQLException;
-//
-//@Ignore
-//public class GekHeadDaoTest extends AbstractDaoTest<GekHeadDao, GekHead, String>
-//{
-//
-//
-//
-//	@Override
-//	protected GekHead getDataset()
-//	{
-//		return new GekHead("qwe", "asd", "zxc");
-//	}
-//
-//	@Override
-//	protected String getPkOfFirst()
-//	{
-//		return "qwe";
-//	}
-//
-//	@Override
-//	protected GekHead getSecondDataset()
-//	{
-//		return new GekHead("test", "test", "test");
-//	}
-//
-//	@Override
-//	protected String getPkOfSecond()
-//	{
-//		return "test";
-//	}
-//
-//	@Override
-//	protected GekHeadDao getDao() throws SQLException
-//	{
-////		return DatabaseService.getInstance().getGekHeadDao();	// do not instantiate directly, because ConnectionPool overflow may occur then
-//
-//		return null;
-//	}
-//
-//	@Override
-//	protected String getNonExistentPk()
-//	{
-//		return "asd";
-//	}
-//
-//
-//}
+package ru.tstu.msword_auto.dao.impl;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.tstu.msword_auto.dao.exceptions.DaoSystemException;
+import ru.tstu.msword_auto.dao.exceptions.NoSuchEntityException;
+import ru.tstu.msword_auto.entity.GekHead;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+public class GekHeadDaoTest {
+
+    // mocks
+    private GekHeadDao dao;
+    private Connection connection;
+    private PreparedStatement statement;
+
+    // entity content
+    private int gekId;
+    private String courseName;
+    private String gekHead;
+    private String gekSubhead;
+    private String gekSecretary;
+
+    // entities
+    private GekHead defaultEntity;
+    private GekHead additionalEntity;
+
+
+    @Before
+    public void setUp() {
+        connection = mock(Connection.class);
+        statement = mock(PreparedStatement.class);
+        dao = new GekHeadDao();
+        dao.connection = this.connection;
+
+        gekId = 1;
+        courseName = "course";
+        gekHead = "head";
+        gekSubhead = "subhead";
+        gekSecretary = "secretary";
+
+        defaultEntity = new GekHead(courseName, gekHead, gekSubhead, gekSecretary);
+        defaultEntity.setGekId(gekId);
+
+        additionalEntity = new GekHead(courseName, gekHead, gekSubhead, gekSecretary);
+        additionalEntity.setGekId(2);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        verify(connection, never()).close();
+    }
+
+
+    @Test
+    public void whenCreateFullEntityThenAllRight() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_CREATE)).thenReturn(statement);
+
+        dao.create(defaultEntity);
+
+        verify(connection).prepareStatement(GekHeadDao.SQL_CREATE);
+        verify(statement).setString(1, courseName);
+        verify(statement).setString(2, gekHead);
+        verify(statement).setString(3, gekSubhead);
+        verify(statement).setString(4, gekSecretary);
+        verify(statement).executeUpdate();
+        verify(statement).close();
+
+    }
+
+    @Test
+    public void whenReadByPrimaryKeyExistingRowThenAllRight() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_BY_PK)).thenReturn(statement);
+        when(statement.executeQuery()).thenReturn(resultSet);
+
+        adjustResultSet(resultSet, true);
+
+        GekHead entity = dao.read(gekId);
+
+        verifyQuery(GekHeadDao.SQL_READ_BY_PK, resultSet, 1, true);
+        verify(resultSet).close();
+        verify(statement).close();
+
+        assertEquals(defaultEntity, entity);
+    }
+
+    @Test
+    public void whenReadByCourseNameExistingRowThenAllRight() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_BY_COURSENAME)).thenReturn(statement);
+        when(statement.executeQuery()).thenReturn(resultSet);
+
+        adjustResultSet(resultSet, true);
+
+        GekHead entity = dao.readByCourseName(courseName);
+
+        verifyQuery(GekHeadDao.SQL_READ_BY_COURSENAME, resultSet, 1, false);
+        verify(resultSet).close();
+        verify(statement).close();
+
+        assertEquals(defaultEntity, entity);
+
+    }
+
+    @Test
+    public void whenReadAllWithMultipleRowsThenAllRight() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_ALL)).thenReturn(statement);
+        when(statement.executeQuery()).thenReturn(resultSet);
+
+        adjustResultSet(resultSet, false);
+
+        List<GekHead> entities = dao.readAll();
+
+        verifyQuery(GekHeadDao.SQL_READ_ALL, resultSet, 2, true);
+        verify(resultSet).close();
+        verify(statement).close();
+
+        GekHead actual1 = entities.get(0);
+        GekHead actual2 = entities.get(1);
+        assertEquals(defaultEntity, actual1);
+        assertEquals(additionalEntity, actual2);
+
+    }
+
+    @Test
+    public void whenUpdateByPkOneRowThenAllRight() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_UPDATE)).thenReturn(statement);
+
+        dao.update(gekId, additionalEntity);
+
+        verify(connection).prepareStatement(GekHeadDao.SQL_UPDATE);
+
+        verify(statement).setString(1, courseName);
+        verify(statement).setString(2, gekHead);
+        verify(statement).setString(3, gekSubhead);
+        verify(statement).setString(4, gekSecretary);
+        verify(statement).executeUpdate();
+        verify(statement).close();
+
+    }
+
+    @Test
+    public void whenDeleteByPkThenAllStepsPerformed() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_DELETE_BY_PK)).thenReturn(statement);
+
+        dao.delete(gekId);
+
+        verify(connection).prepareStatement(GekHeadDao.SQL_DELETE_BY_PK);
+        verify(statement).setInt(1, gekId);
+        verify(statement).executeUpdate();
+        verify(statement).close();
+
+    }
+
+    @Test
+    public void whenDeleteAllThenAllActionsPerformed() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_DELETE_ALL)).thenReturn(statement);
+
+        dao.deleteAll();
+
+        verify(connection).prepareStatement(GekHeadDao.SQL_DELETE_ALL);
+        verify(statement).executeUpdate();
+        verify(statement).close();
+
+    }
+
+    @Test(expected = NoSuchEntityException.class)
+    public void whenReadByPrimaryKeyNonExistingRowThenException() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_BY_PK)).thenReturn(statement);
+        when(statement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+
+        GekHead entity = dao.read(gekId);
+
+        verify(connection).prepareStatement(GekHeadDao.SQL_READ_BY_PK);
+        verify(statement).setInt(1, gekId);
+        verify(statement).executeQuery();
+        verify(resultSet).next();
+        verify(resultSet).close();
+        verify(statement).close();
+
+        assertNull(entity);
+    }
+
+    @Test(expected = NoSuchEntityException.class)
+    public void whenReadByCourseNameNonExistingRowThenException() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_BY_COURSENAME)).thenReturn(statement);
+        when(statement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+
+        GekHead entity = dao.readByCourseName(courseName);
+
+        verify(connection).prepareStatement(GekHeadDao.SQL_READ_BY_COURSENAME);
+        verify(statement).setInt(1, gekId);
+        verify(statement).executeQuery();
+        verify(resultSet).next();
+        verify(resultSet).close();
+        verify(statement).close();
+
+        assertNull(entity);
+    }
+
+    @Test(expected = DaoSystemException.class)
+    public void whenCreateThrowsSqlExceptionThenThrownDaoSystemException() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_CREATE)).thenReturn(statement);
+        when(statement.executeUpdate()).thenThrow(SQLException.class);
+
+        dao.create(defaultEntity);
+    }
+
+    @Test(expected = DaoSystemException.class)
+    public void whenReadByPkThrowsSqlExceptionThenThrownDaoSystemException() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_BY_PK)).thenReturn(statement);
+        when(statement.executeQuery()).thenThrow(SQLException.class);
+
+        dao.read(gekId);
+    }
+
+    @Test(expected = DaoSystemException.class)
+    public void whenReadByCourseNameThrowsSqlExceptionThenThrownDaoSystemException() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_BY_COURSENAME)).thenReturn(statement);
+        when(statement.executeQuery()).thenThrow(SQLException.class);
+
+        dao.readByCourseName(courseName);
+    }
+
+    @Test(expected = DaoSystemException.class)
+    public void whenReadAllThrowsSqlExceptionThenThrownDaoSystemException() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_READ_ALL)).thenReturn(statement);
+        when(statement.executeQuery()).thenThrow(SQLException.class);
+
+        dao.readAll();
+    }
+
+    @Test(expected = DaoSystemException.class)
+    public void whenUpdateThrowsSqlExceptionThenThrownDaoSystemException() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_UPDATE)).thenReturn(statement);
+        when(statement.executeUpdate()).thenThrow(SQLException.class);
+
+        dao.update(gekId, defaultEntity);
+    }
+
+    @Test(expected = DaoSystemException.class)
+    public void whenDeleteByPkThrowsSqlExceptionThenThrownDaoSystemException() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_DELETE_BY_PK)).thenReturn(statement);
+        when(statement.executeUpdate()).thenThrow(SQLException.class);
+
+        dao.delete(gekId);
+    }
+
+    @Test(expected = DaoSystemException.class)
+    public void whenDeleteAllThrowsSqlExceptionThenThrownDaoSystemException() throws Exception {
+        when(connection.prepareStatement(GekHeadDao.SQL_DELETE_ALL)).thenReturn(statement);
+        when(statement.executeUpdate()).thenThrow(SQLException.class);
+
+        dao.deleteAll();
+    }
+
+
+    private void adjustResultSet(ResultSet resultSet, boolean oneTime) throws Exception {
+
+        if(oneTime) {
+            when(resultSet.next())
+                    .thenReturn(true)
+                    .thenReturn(false);
+
+            when(resultSet.getInt(GekHeadDao.TABLE_GEK_ID)).thenReturn(gekId);
+        } else {    // pk many(at least 2)
+            when(resultSet.next())
+                    .thenReturn(true)
+                    .thenReturn(true)
+                    .thenReturn(false);
+
+            when(resultSet.getInt(GekHeadDao.TABLE_GEK_ID))
+                    .thenReturn(gekId)
+                    .thenReturn(additionalEntity.getGekId()); // second id, second entity
+        }
+
+        when(resultSet.getString(GekHeadDao.TABLE_COURSE_NAME)).thenReturn(courseName);
+        when(resultSet.getString(GekHeadDao.TABLE_GEK_HEAD)).thenReturn(gekHead);
+        when(resultSet.getString(GekHeadDao.TABLE_GEK_SUBHEAD)).thenReturn(gekSubhead);
+        when(resultSet.getString(GekHeadDao.TABLE_GEK_SECRETARY)).thenReturn(gekSecretary);
+
+    }
+
+    private void verifyQuery(String sql, ResultSet resultSet, int times, boolean byPrimaryKey) throws Exception {
+        verify(connection).prepareStatement(sql);
+
+        // if by pk
+        if(times == 1 && byPrimaryKey) {
+            verify(statement).setInt(1, gekId);
+        } else if(times == 1 && !byPrimaryKey) {
+            verify(statement).setString(1, courseName);
+        }
+        verify(statement).executeQuery();
+
+        int resultSetNextTimes = times + 1; // because there would be last, that returns false
+        verify(resultSet, times(resultSetNextTimes)).next();
+        verify(resultSet, times(times)).getString(GekHeadDao.TABLE_COURSE_NAME);
+        verify(resultSet, times(times)).getString(GekHeadDao.TABLE_GEK_HEAD);
+        verify(resultSet, times(times)).getString(GekHeadDao.TABLE_GEK_SUBHEAD);
+        verify(resultSet, times(times)).getString(GekHeadDao.TABLE_GEK_SECRETARY);
+
+    }
+
+
+
+}
